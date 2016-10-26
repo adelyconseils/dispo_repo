a5969e83609d7720cd3bb869a31c8ef86b97c08a
adelyconseils <aymen.boubaker@adelyconseils.com>
2016-10-26 23:02:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var api = new ParseServer({
   push: {
     ios: {
       pfx: 'DispoDevCertificats.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk  
-      passphrase: 'Taraji1919', // optional password to your p12
+      passphrase: 'taraji1919', // optional password to your p12
       bundleId: 'com.adelyconseils.dispo', // The bundle identifier associate with your app
       production: false // Specifies which environment to connect to: Production (if true) or Sandbox
     }
